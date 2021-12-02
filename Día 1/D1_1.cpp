4b058b198ed80866d67b16a92548b2b37956b7c7
jamartbr <79722449+jamartbr@users.noreply.github.com>
2021-12-02 12:53:38 +0100

    Update D1_1.cpp

--- Día 1/D1_1.cpp
+++ Día 1/D1_1.cpp
@@ -1,1 +1,22 @@
-hola
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int primero, segundo, suma=0;
+    
+    cin >> primero;
+    
+    for (int i=0; i<1999; i++) {
+        cin >> segundo;
+        if(segundo>primero) {
+            suma++;
+        }
+        primero = segundo;
+    }
+    
+    cout << suma;
+
+    return 0;
+}
