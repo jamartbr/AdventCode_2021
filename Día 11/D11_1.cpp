03d44d13561f9551f717c00568c9532b66d3ddc3
jamartbr <79722449+jamartbr@users.noreply.github.com>
2021-12-11 10:26:34 +0100

    Create D11_1.cpp

---
+++ Día 11/D11_1.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Mapa {
+  
+private:
+    
+    int mapa[10][10];
+    bool brilla[10][10];
+  
+public:
+    
+    Mapa () {
+        for (int i=0; i<10; i++) {
+            string cad;
+            cin >> cad;
+            for (int j=0; j<10; j++) {
+                mapa[i][j] = cad.at(j)-48;
+                brilla[i][j] = false;
+            }
+        }
+    }
+    
+    int get (int i, int j) {
+        if (i>=0 && i<10 && j>=0 && j<10)
+            return mapa[i][j];
+        else
+            return 0;
+    }
+    
+    void aumentar (int i, int j) {
+        if (i>=0 && i<10 && j>=0 && j<10)
+            mapa[i][j]++;
+    }
+    
+    int turno () {
+        int suma = 0;
+        for (int i=0; i<10; i++) {
+            for (int j=0; j<10; j++) {
+                if (mapa[i][j]!=9)
+                    mapa[i][j]++;
+                else
+                    suma += encender(i,j);
+            }
+        }
+        apagar();
+        return suma;
+    }
+    
+    int encender (int i, int j) {
+        int suma = 1;
+        brilla[i][j] = true;
+        mapa[i][j] = 0;
+        
+        for (int x=i-1; x<=i+1; x++) {
+            for (int y=j-1; y<=j+1; y++) {
+                aumentar(x,y);
+                if (get(x,y)>9) {
+                    suma += encender(x,y);
+                }
+            }
+        }
+        return suma;
+    }
+    
+    void apagar () {
+        for (int i=0; i<10; i++) {
+            for (int j=0; j<10; j++) {
+                if (brilla[i][j]) {
+                    brilla[i][j] = false;
+                    mapa[i][j] = 0;
+                }
+            }
+        }
+    }
+};
+
+int main()
+{
+    Mapa mapa;
+    
+    int suma = 0;
+    
+    for (int i=0; i<100; i++) {
+        suma += mapa.turno();
+    }
+    
+    cout << suma;
+    
+    return 0;
+}
